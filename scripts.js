f772554fdd8157d335cdd9bb403f712b4a33bf7a
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-11 20:33:09 +0200

    5 Exercise

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const tab = [1, 4, 'Iwona', false, 'Nowak'];
 
-const average = (...args) => {
- 	let sum = 0;
-  	args.forEach(arg => sum += arg); 
-  	console.log(sum/[...args].length);
-};
+const [,,firstName,,secondName] = tab;
 
-average(...grades);
+console.log(`${firstName} ${secondName}`);
