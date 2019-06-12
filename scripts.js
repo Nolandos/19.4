2f066ff11ef9bdd4d384a18b5382420e22dbe4f3
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-12 12:48:34 +0200

    4 Extra-Exercise

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
-const sum = (arr) => {
-  let total = 0; 
-  arr.forEach(item => total += item);
-  
-  return total;
+function spread(func, args) {
+  return func(...args);
 }
 
-var test = [[1,2,3,4,5,6,7,8,9,10],[71,-548,12.3,83,-47,-1.7,-892,58,31415,2718,-38]];
-
-sum(test[0]);
-
-sum(test[1]);
-
-//Tutaj mi wyskakuje błąd na stronie codewars, nie rozumiem o co im dokładnie chodzi :/
