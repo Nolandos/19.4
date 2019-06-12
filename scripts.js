69841c33ebc08fe5dc78e3916b3d862a0bcc1691
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-12 12:42:15 +0200

    3 Extra-Exercise

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const sum = (arr) => {
   let total = 0; 
   arr.forEach(item => total += item);
-  console.log(total);
+  
+  return total;
 }
 
 var test = [[1,2,3,4,5,6,7,8,9,10],[71,-548,12.3,83,-47,-1.7,-892,58,31415,2718,-38]];
