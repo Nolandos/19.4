f2dd958bb9acdd7cf15c1c1456f47d992a7c5cc8
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-12 11:41:01 +0200

    2 Extra-Exercise

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-function buildString(...template){
-  return `I like ${template.join(', ')}!`;
-}
+const shuffleIt  = (arr, ...rest) => {
+  for(let i=0; i<rest.length; i++) {
+    [arr[rest[i][0]],arr[rest[i][1]]] = [arr[rest[i][1]],arr[rest[i][0]]];
+  }
+  return arr;
+} 
+
+//Testy przeszło, nawet te 100 losowych... :D 
